e8b467926b7c4104dc471898cedc776d6d41d7e8
Dima Panov <fluffy@FreeBSD.org>
2022-01-27 17:45:06 +0300

    devel/py-sip: delete outdated patches

--- devel/py-sip/files/patch-sipbuild_py__versions.py
+++
@@ -1,8 +0,0 @@
---- sipbuild/py_versions.py.orig	2020-11-23 13:21:33 UTC
-+++ sipbuild/py_versions.py
-@@ -23,4 +23,4 @@
- 
- # The first and last supported minor versions of Python v3.
- FIRST_SUPPORTED_MINOR = 5
--LAST_SUPPORTED_MINOR = 9
-+LAST_SUPPORTED_MINOR = 11

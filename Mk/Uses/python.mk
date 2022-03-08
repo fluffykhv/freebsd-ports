c6053fd88a4846fe4a085041e3fe240b88021a0c
Dima Panov <fluffy@FreeBSD.org>
2022-03-08 21:41:03 +0300

    Revert "Mk/Uses/python.mk: devel/cython now supports py311"
    This reverts commit 61427ae2f94b04ec06db8458c6217d78518ba12f.

--- Mk/Uses/python.mk
+++ Mk/Uses/python.mk
@@ -514,11 +514,11 @@ _PYTHONPKGLIST=	${WRKDIR}/.PLIST.pymodtmp
 # - it uses USE_PYTHON=distutils
 #
 
-#.if ${PYTHON_REL} >= 31100
-#_CYTHON_DEP=	cython-${PYTHON_VER}:lang/cython-devel@${PY_FLAVOR}
-#.else
+.if ${PYTHON_REL} >= 31100
+_CYTHON_DEP=	cython-${PYTHON_VER}:lang/cython-devel@${PY_FLAVOR}
+.else
 _CYTHON_DEP=	cython-${PYTHON_VER}:lang/cython@${PY_FLAVOR}
-#.endif
+.endif
 
 .if defined(_PYTHON_FEATURE_CYTHON)
 BUILD_DEPENDS+=	${_CYTHON_DEP}

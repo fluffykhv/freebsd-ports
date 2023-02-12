76c7d7dcbc9e3f251816a79261f22b99b1d4ff59
Dima Panov <fluffy@FreeBSD.org>
2023-02-12 21:49:03 +0300

    Revert "devel/boost-all: aarch64 still affected to aassertions with base llvm15"
    This reverts commit 4f49bf5364a72495f15930ca25768ee788a11e5c.

--- devel/boost-all/compiled.mk
+++ devel/boost-all/compiled.mk
@@ -43,7 +43,7 @@ MAKE_ARGS+=	pch=off
 
 .include <bsd.port.options.mk>
 
-.if ${OPSYS} == FreeBSD && ${OSVERSION} >= 1400000 && (${OSVERSION} < 1400079 || ${ARCH} == aarch64)
+.if ${OPSYS} == FreeBSD && ${OSVERSION} >= 1400000 && ${OSVERSION} < 1400079
 USES+=	llvm:build
 CC=	${_LLVM_MK_PREFIX}/bin/clang
 CPP=	${_LLVM_MK_PREFIX}/bin/clang-cpp
